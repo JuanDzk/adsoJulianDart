c20e4ed3626c3899f88f18d38889568e14c0504a
Dahiana18 <dahianarave18@gmail.com>
2024-05-07 08:37:36 -0500

    ciclos

---
+++ ciclos/ejemplosCiclos/ejemploFor01.dart
@@ -0,0 +1,13 @@
+void main(List<String> alg){
+  //Cuando se requiere que una accion se repita (n) numero de veces [determinado{FOR} // indeterminado {WHILE / DO WHILE}
+
+  /*Estructura FOR (para):
+  numero definido de veces [por el problema o el usuario]
+  PARTES: valor inicial, valor final, incremento
+  SINTAXIS: for(int i=0; i<=5; i++ [=incremento {i=i+1}])
+  */
+  print("imprimir los numeros del 1 al 5");
+  for(int i=0; i<=5; i++){
+    print("i: $i");
+  }
+}
