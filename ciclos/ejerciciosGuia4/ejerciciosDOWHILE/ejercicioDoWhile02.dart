7dc2c3abdb6bc30fb7d491e75f25111f40763b96
JuanDzk <juandiegocalvoosorio@gmail.com>
2024-05-26 12:38:30 -0800

    correccion2

--- ciclos/ejerciciosGuia4/ejerciciosDOWHILE/ejercicioDoWhile02.dart
+++ ciclos/ejerciciosGuia4/ejerciciosDOWHILE/ejercicioDoWhile02.dart
@@ -1,11 +1,26 @@
-
+import 'dart:io';
 
 void main(List<String> args) {
- //JUAN DIEGO CALVO OSORIO
+  //Ejercicio 02-Maria Paula Melo Solano
+  /*
+  Un alumno de la clase de lógica matemática desea desarrollar un algoritmo y diagrama de flujo en el cual  introduzca 
+  un  número  entero  positivo  e  invierta  los  dígitos  del  número.  Mostrar  el  número invertido.
+  */
 
-  /* Un alumno de la clase de lógica matemática desea desarrollar un algoritmo y diagrama de flujo en el
-cual introduzca un número entero positivo e invierta los dígitos del número. Mostrar el número
-invertido.*/
-
-
+  //DEFINIR
+  int numInicial;
+  int newNum, modulo, division;
+  //ENTRADA
+  print("Ingrese el número deseado a invertir");
+  numInicial = int.parse(stdin.readLineSync()!);
+  newNum = numInicial;
+  //PROCESO
+  print("El número inicial es: $numInicial");
+  stdout.write("El número invertido es: ");
+  do {
+    modulo = newNum % 10;
+    division = newNum ~/ 10;
+    stdout.write(modulo);
+    newNum = division;
+  } while (division != 0);
 }
