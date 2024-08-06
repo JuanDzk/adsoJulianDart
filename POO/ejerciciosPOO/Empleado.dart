15850ec6d9b4d65a15bb7b28c353d56e452d4acb
Alejo-serna <alejandrokfjd@gmail.com>
2024-08-06 09:30:06 -0500

    taller POO 100%

--- POO/ejerciciosPOO/Empleado.dart
+++ POO/ejerciciosPOO/Empleado.dart
@@ -1,3 +1,5 @@
+
+
 class Empleado {
   String nombre;
   int edad;
@@ -10,35 +12,43 @@ class Empleado {
   Empleado(
       this.nombre, this.edad, this.salario, this.puesto, this.tipoContrato);
 
-  //metodo aumentar salario
-  void aumentarSalario(double porcentaje) {
+  //metodo aumentar salario (gud)
+  double aumentarSalario(double porcentaje) {
     this.salario += (this.salario * porcentaje) / 100;
+    return this.salario;
   }
 
-  //metodo cumplir años
-  void cumplirAnios() {
+  //metodo cumplir años (gud)
+  int cumplirAnios() {
     this.edad++;
+    return this.edad;
   }
 
-  //metodo cambiar de puesto
-  void cambiarPuesto(String nuevoPuesto) {
+  //metodo cambiar de puesto (gud)
+  String cambiarPuesto(String nuevoPuesto) {
     this.puesto = nuevoPuesto;
+    return this.puesto;
   }
 
   //metodo mostrar informacion
-  void mostrarInformacion() {
+   mostrarInformacion() {
     print(
-        'Nombre: $nombre, Edad: $edad, Salario: $salario, Puesto: $puesto, Tipo de contrato: $tipoContrato');
+        """Nombre: $nombre.
+           Edad: $edad.
+           Salario: $salario.
+           Puesto: $puesto. 
+           Tipo de contrato: $tipoContrato.
+        """);
   }
 
   //metodo calcular bonificacion
   double calcularBonificacion() {
-    if (this.puesto == 'contratista') {
-      return this.salario * 0.1;
-    } else if (this.puesto == 'temporal') {
-      return this.salario * 0.05;
-    } else if (this.puesto == 'indefinido') {
-      return this.salario * 0.15;
+    if (this.tipoContrato.toLowerCase() == 'contratista') {
+      return this.salario = this.salario + this.salario * 0.1;
+    } else if (this.tipoContrato.toLowerCase() == 'temporal') {
+      return this.salario = this.salario + this.salario * 0.05;
+    } else if (this.tipoContrato.toLowerCase() == 'indefinido') {
+      return this.salario = this.salario + this.salario * 0.15;
     } else {
       return 0;
     }
--- POO/ejerciciosPOO/ejercicioPOO.dart
+++ POO/ejerciciosPOO/ejercicioPOO.dart
@@ -1,20 +1,71 @@
+
 import 'dart:io';
 
 import 'Empleado.dart';
 
 void main(List<String> args) {
   List<Empleado> listaEmpleados = [];
-  String nombreUsuario;
   int cantidadEntradas;
+  String nombreUsuario;
   int edadUsuario;
-  double salarioUsuario;
+  double salarioUsuario = 0;
   String puestoUsuario;
   String tipoContratoUsuario;
 
   print("cantidad de entradas:");
   cantidadEntradas = int.parse(stdin.readLineSync()!);
   for (int i = 0; i < cantidadEntradas; i++) {
-    print("ingrese el nombre del empleado ${i+1}");
+    print("ingrese el nombre del empleado ${i + 1}");
     nombreUsuario = stdin.readLineSync()!;
+    print("ingrese la edad del usuario ${i + 1}");
+    edadUsuario = int.parse(stdin.readLineSync()!);
+    print("ingrese el salario del usuario ${i + 1}");
+    salarioUsuario = double.parse(stdin.readLineSync()!);
+    print("ingrese el puesto del usuario ${i + 1}");
+    puestoUsuario = stdin.readLineSync()!;
+    print("ingrese el tipo de contrato del empleado ${i + 1}");
+    tipoContratoUsuario = stdin.readLineSync()!;
+
+//se crea el objeto de clase empleado.
+    Empleado objEmpleado = Empleado(nombreUsuario, edadUsuario, salarioUsuario,
+        puestoUsuario, tipoContratoUsuario);
+
+// se añade a la lista
+    listaEmpleados.add(objEmpleado);
   }
+
+// ejecutar los metodos
+
+
+for (var i = 0; i < listaEmpleados.length; i++) {
+  print("EL EMPLEADO ${i+1}");
+
+
+  //METODO MOSTRAR INFORMACION
+  listaEmpleados[i].mostrarInformacion();
+
+
+  //METODO AUMENTO DE SALARIO
+  print("porcentaje de salario aumentado");
+  double newAumento = double.parse(stdin.readLineSync()!);
+  double newSalario = listaEmpleados[i].aumentarSalario(newAumento);
+  print("el nuevo salario aumentado es: $newSalario");
+  
+  //METODO CUMPLIR AÑOS
+  int newEdad = listaEmpleados[i].cumplirAnios();
+  print("feliz cumpleaños numero: $newEdad");
+
+  //METODO CAMBIAR PUESTO
+  print("el empleado  cambia de puesto, digite su nuevo puesto");
+  String puestoNew= stdin.readLineSync()!;
+  String newPuesto = listaEmpleados[i].cambiarPuesto(puestoNew);
+  print("el nuevo puesto es: $newPuesto");
+
+  //METODO PARA BONIFICACION
+  double newBonificacion = listaEmpleados[i].calcularBonificacion();
+  print("la bonificacion del salario: $newSalario es: $newBonificacion");
+
+  //METODO MOSTRAR INFO
+  listaEmpleados[i].mostrarInformacion();
+}
 }
